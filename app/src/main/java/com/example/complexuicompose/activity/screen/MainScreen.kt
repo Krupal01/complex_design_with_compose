480e3957767a1059647b1eafe1a2392a682f29dd
krupal <bhuvakrupal01@gmail.com>
2022-06-24 16:32:41 +0530

    ADD : Motion Layout

--- app/src/main/java/com/example/complexuicompose/activity/screen/MainScreen.kt
+++ app/src/main/java/com/example/complexuicompose/activity/screen/MainScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.complexuicompose.activity.composable.MotionLayoutCompose
 import com.example.complexuicompose.activity.composable.SwipeItemLazyColumn
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -19,32 +20,34 @@ import com.example.complexuicompose.activity.composable.SwipeItemLazyColumn
 fun MainScreen(
     navController: NavController
 ) {
-    
-    val listm = remember {
-        mutableStateListOf("sd","Sfd","sdfsd","sfsd")
-    }
-    SwipeItemLazyColumn(
-        modifier = Modifier.fillMaxWidth().padding(all = 2.dp),
-        items = listm,
-        itemCompose = {
-            Text(text = it.toString(), modifier = Modifier.fillMaxWidth())
-        },
-        directions = setOf(DismissDirection.EndToStart, DismissDirection.StartToEnd),
-        swipeAction = {
-            when(it.dismissDirection){
-                DismissDirection.StartToEnd -> {
-                    Log.i("action","start to end")
-                }
-                DismissDirection.EndToStart -> {
-                    Log.i("action","end to start")
-                }
-                null -> {
-                    Log.i("action","null")
-                }
 
-            }
-        }
-    )
+    MotionLayoutCompose()
+
+//    val listm = remember {
+//        mutableStateListOf("sd","Sfd","sdfsd","sfsd")
+//    }
+//    SwipeItemLazyColumn(
+//        modifier = Modifier.fillMaxWidth().padding(all = 2.dp),
+//        items = listm,
+//        itemCompose = {
+//            Text(text = it.toString(), modifier = Modifier.fillMaxWidth())
+//        },
+//        directions = setOf(DismissDirection.EndToStart, DismissDirection.StartToEnd),
+//        swipeAction = {
+//            when(it.dismissDirection){
+//                DismissDirection.StartToEnd -> {
+//                    Log.i("action","start to end")
+//                }
+//                DismissDirection.EndToStart -> {
+//                    Log.i("action","end to start")
+//                }
+//                null -> {
+//                    Log.i("action","null")
+//                }
+//
+//            }
+//        }
+//    )
 
 
 }
